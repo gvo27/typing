0b7f14483c8bb58e858caccc2165312df9f2e706
gvo <gvo@berkeley.edu>
2024-10-03 23:10:50 -0700

    cleaning up

--- num.py
+++ num.py
@@ -11,33 +11,47 @@ class IterObj:
         pass
 
 class Number(IterObj):
-    def __init__(self, max_len: int = 9, complex: bool = True, max_dec: int = 5):
-        self.complex = complex
+    def __init__(self, max_len: int = 9, comma: bool = True, max_dec: int = 4): 
+        super().__init__()
+        self.comma = comma
         self.max_len = max_len
         self.max_dec = max_dec
     
     def __next__(self) -> str:
         num = self.rand()
-        if self.complex:
+        if self.comma:
             return f"{num:,}"
         else:
-            return f"{int(num)}"
+            return f"{num}"
     
     def rand(self) -> float:
         length = random.randint(self.max_len//2, self.max_len)
         if length < 1:
             return 0
-        dec = random.randint(2, self.max_dec) 
-        return round(random.uniform(0, 10**length), dec) 
+        result = random.uniform(0, 10**length)
+        if self.max_dec == 0:
+            return int(result)
+        dec = random.randint(1, self.max_dec) 
+        return round(result, dec) 
     
 class Sentence(IterObj):
-    def __init__(self, num_words: int = 3, word_size: int = 9, complex_words: bool = True):
-        self.length = num_words
-        self.num = Number(max_len=word_size, complex=complex_words)
+    def __init__(self, iterator: IterObj, num_words: int = 3):
+        super().__init__()
+        self.iterator = iterator
+        self.len = num_words
     
     def __next__(self) -> str:
         result = ""
-        for _ in range(self.length):
-            n = next(self.num)
+        for _ in range(self.len):
+            n = next(self.iterator)
             result += n + ' '
         return result
+
+class Generator(IterObj):
+    def __init__(self, word_size: int, thousandth_comma: bool, decimal_places: int, sent_size: int):
+        super().__init__()
+        self.num = Number(max_len=word_size, comma=thousandth_comma, max_dec= decimal_places)
+        self.sent = Sentence(iterator=self.num, num_words=sent_size)
+                
+    def __next__(self) -> str:
+        return next(self.sent)
--- run.py
+++ run.py
@@ -1,14 +1,11 @@
 import datetime
-from num import Sentence
+from num import Generator
 from difflib import SequenceMatcher
 
 def run():
     
-    NUMBER_OF_WORDS = 7
-    COMMAS_AND_DECIMALS = True
-    
-    intro = input("Press Enter to begin.") 
-    generator = Sentence(num_words=NUMBER_OF_WORDS, complex_words=COMMAS_AND_DECIMALS)
+    intro = input("...Press Enter to begin...") 
+    generator = Generator(9, True, 4, 18)
     while True:
         example = next(generator)
         start = datetime.datetime.now()
@@ -17,7 +14,7 @@ def run():
         time = round((datetime.datetime.now() - start).total_seconds(), 2) # seconds
         result = check(example, answer)
         kph = round(len(answer) / time * 60 * 60, 2)
-        input(f"Accuracy: {int(result*100)}% \nTime: {time} s \nKPH: {kph} \nPress Enter to continue.")
+        input(f">Accuracy: {int(result*100)}% \n>Time: {time} s \n>KPH: {kph} \n...Press Enter to continue...")
 
 def check(a, b):
     s = SequenceMatcher(None, a, b)
